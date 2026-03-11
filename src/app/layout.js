3b89f4e225094080368ebb183aab7dc6f84b48d7
Jonathan Stern <jstern718@gmail.com>
2026-03-11 16:16:09 -0400

    same

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${playfair.variable} bg-green-dark text-white`}>
+      <body className={`${inter.variable} ${playfair.variable} bg-primary text-white`}>
         {children}
       </body>
     </html>
